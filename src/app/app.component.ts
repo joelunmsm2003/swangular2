2b52141a72f247af08b1b3ed593007751166a006
Joel Andy <joel@waqastudios.com>
2017-07-10 00:36:29 -0500

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {PruebaService} from './prueba.service';
+import {Http, Response} from '@angular/http';
+import { Person } from './person';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+   people: Person[] = [];
+  errorMessage: string = '';
+  isLoading: boolean = true;
+message: string;
+
+
+
+
+  constructor(private http : Http,private _pruebaService: PruebaService) { }
+
+
+ ngOnInit(){
+    
+    this.message = '';
+    this.http.get('http://estokealo.com:8000/productosjson')
+      .map(res => res.json())
+      .subscribe(
+
+        data =>  console.log('sjjjsjs',data),
+        error => this.message = error._body
+      );
+ 
+  }
+
+ 
 }
+
---
+++ src/app/person.ts
@@ -0,0 +1,6 @@
+export interface Person {
+  id: number;
+  photo__photo: string;
+  detalle: string;
+
+}
